f5e51e9b0c5e5c6cc7f5f8fd328c2da73400edde
Tim Thomas <thomasti@oregonstate.edu>
2016-05-22 22:26:58 -0400

    CS 290 final project initial commit

---
+++ final_proj/workout.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var session = require('express-session');
+var request = require('request');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({secret:'SuperSecretPassword'}));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res,next){
+    var context = {};
+    res.render('workout',context);
+});
+
+
+app.post('/', function(req,res){
+    var context = {};
+    res.render('workout',context);
+});
+
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
